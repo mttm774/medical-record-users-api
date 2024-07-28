41048ab25a3a355955de37208d68485c3cd6c46a
zearkiatos <caprilespe@outlook.com>
2024-07-28 00:43:34 -0400

    Refactoring code with users patients and starting work omn update

--- docker/create_db.sql
+++ docker/create_db.sql
@@ -42,3 +42,63 @@ ALTER TABLE [dbo].[User] ADD  CONSTRAINT [UK_UserName] UNIQUE NONCLUSTERED
 	[UserName] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
+
+CREATE TABLE [dbo].[Usuario](
+	[Id] [numeric](18, 0) IDENTITY(1,1) NOT NULL,
+	[TipoDocumento] [varchar](2) NOT NULL,
+	[NumeroDocumento] [varchar](20) NOT NULL,
+	[PrimerApellido] [varchar](50) NOT NULL,
+	[SegundoApellido] [varchar](50) NULL,
+	[PrimerNombre] [varchar](50) NOT NULL,
+	[SegundoNombre] [varchar](50) NULL,
+	[EstadoCivil] [varchar](20) NULL,
+	[FechaNacimiento] [datetime] NULL,
+	[Sexo] [varchar](2) NULL,
+	[Ocupacion] [varchar](50) NULL,
+	[Pais] [varchar](50) NULL,
+	[Departamento] [varchar](50) NULL,
+	[Municipio] [varchar](50) NULL,
+	[BarrioVereda] [varchar](50) NULL,
+	[Zona] [varchar](1) NULL,
+	[Direccion] [varchar](100) NULL,
+	[Estrato] [varchar](20) NULL,
+	[TelefonoContacto] [varchar](50) NULL,
+	[TelefonoResidencia] [varchar](50) NULL,
+	[NombresPadre] [varchar](50) NULL,
+	[NombresMadre] [varchar](50) NULL,
+	[NombresAcudiente] [varchar](50) NULL,
+	[TelefonoPadres] [varchar](50) NULL,
+	[TelefonoAcudiente] [varchar](50) NULL,
+	[NombreEPS] [varchar](100) NULL,
+	[CodigoEPS] [varchar](10) NULL,
+	[Regimen] [varchar](1) NULL,
+	[NombreIPS] [varchar](100) NULL,
+	[CodigoIPS] [varchar](20) NULL,
+	[TipoIdentificacionIPS] [varchar](2) NULL,
+	[Discapacidad] [varchar](100) NULL,
+	[GrupoEtnico] [varchar](50) NULL,
+	[Observaciones] [varchar](max) NULL,
+	[Foto] [image] NULL,
+	[Email] [varchar](100) NULL,
+	[Estado] [varchar](2) NOT NULL,
+	[localidad] [varchar](50) NULL,
+	[Area] [varchar](100) NULL,
+	[Posicion] [varchar](100) NULL,
+	[AceptacionConsentimiento] [varchar](1) NULL,
+	[AceptacionDatosPersonales] [varchar](1) NULL,
+	[ParentescoAcudiente] [varchar](50) NULL,
+	[recuperacion] [varchar](2) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Usuario] ADD  CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+ALTER TABLE [dbo].[Usuario] ADD  CONSTRAINT [UK_NumDocUsuario] UNIQUE NONCLUSTERED 
+(
+	[NumeroDocumento] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
--- medical-record-users-api/Controllers/UsersController.cs
+++ medical-record-users-api/Controllers/UsersController.cs
@@ -23,38 +23,36 @@ namespace medical_record_users_api.Controllers
      
         // GET: api/values
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsers()
         {
-            var users = await context.Users.ToListAsync();
+            var users = await context.Usuarios.ToListAsync();
             return Ok(users);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> Get(int id)
+        public async Task<ActionResult<Usuario>> Get(int id)
         {
-            Console.WriteLine(id);
-            var user = await context.Users.FirstOrDefaultAsync<User>(x=>x.Id == id);
+            var user = await context.Usuarios.FirstOrDefaultAsync<Usuario>(x=>x.Id == id);
 
             if (user != null) {
                 return user;
             }
             else {
-                return NotFound("User doesn't find");
+                return NotFound("Patient doesn't find");
             }
         }
 
         // POST api/values
         [HttpPost]
-        public async Task<ActionResult<User>> Post([FromBody]User user)
+        public async Task<ActionResult<User>> Post([FromBody]Usuario user)
         {
-            Console.WriteLine(user.Email);
             try {
                 if (user == null) 
                 {
-                    return BadRequest("The user is empty");
+                    return BadRequest("The patient is empty");
                 }
-                context.Users.Add(user);
+                context.Usuarios.Add(user);
                 await context.SaveChangesAsync();
 
                 return CreatedAtAction(nameof(Get), new {id = user.Id}, user);
@@ -71,8 +69,32 @@ namespace medical_record_users_api.Controllers
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<ActionResult<User>> Put(int id, [FromBody]User user)
         {
+            try {
+                if (user == null) 
+                {
+                    return BadRequest("The patient is empty");
+                }
+                var userForUpdate = await context.Users.FirstOrDefaultAsync<User>(x=>x.Id == id);
+                if (userForUpdate == null ) {
+                    return NotFound("Patient doesn't find");
+                }
+                userForUpdate.Email = user.Email;
+                userForUpdate.Estado = user.Estado;
+                userForUpdate.Firma = user.Firma;
+                userForUpdate.Intentos = user.Intentos;
+                await context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(Get), new {id = user.Id}, user);
+            }
+            catch(Exception ex) {
+                Console.WriteLine($"Error: {ex.Message} {ex.StackTrace}");
+                return StatusCode(500, new {
+                    message = "Internal server error something is wrong",
+                    errorMessage = ex.Message
+                });
+            }
         }
 
         // DELETE api/values/5
